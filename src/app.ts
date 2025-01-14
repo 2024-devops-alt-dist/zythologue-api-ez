724571f5c22ebba245b9ab4ace2cc1f95679795d
elena <zianielena@gmail.com>
2025-01-14 10:19:42 +0100

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,17 @@ import express, { Application } from "express";
 const app: Application = express();
 
 import { setupSwagger } from "../swagger";
+import cors from 'cors';
 
 const version = "v1";
 const path = `/api/${version}`;
 
 import { router as beersRoutes } from "./routes/beers";
 import { router as breweriesRoutes } from "./routes/breweries";
+
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
 
 // Middleware to parse json in requests
 app.use(express.json());
